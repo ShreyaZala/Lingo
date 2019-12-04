31d880b7de547cbe496285b5049437f37e2117ed
Shreya <zalas@sunypoly.edu>
2019-12-03 20:43:54 -0500

    Intial commit

--- Lingo/ViewController.swift
+++ Lingo/ViewController.swift
@@ -1,18 +1,42 @@
-//
-//  ViewController.swift
-//  Lingo
-//
-//  Created by Shreya Jhala on 12/3/19.
-//  Copyright © 2019 Shreya Zala. All rights reserved.
-//
-
 import UIKit
+import CoreML
+import Vision
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    let imagePicker = UIImagePickerController()
+    
+    @IBAction func cameraButton(_ sender: UIBarButtonItem) {
+        
+        present(imagePicker, animated: true, completion: nil)
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingWithMediaInfo info: [String: Any]) {
+        
+        if let userPickedImage = [UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            
+            imageView.image = userPickedImage
+            
+        }
+        
+        imagePicker.dismiss(animated: true, completion: nil)
+        
+    }
+    
 
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        imagePicker.delegate = self
+        
+        imagePicker.sourceType = .camera
+        
+        imagePicker.allowsEditing = false
+        
     }
 
 
