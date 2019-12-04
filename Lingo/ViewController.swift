5d57c03a0fe7b8fd4a2b529e296975d767c3d6f8
Shreya <zalas@sunypoly.edu>
2019-12-03 19:04:19 -0500

    Initial Commit

---
+++ Lingo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lingo
+//
+//  Created by Shreya Jhala on 12/3/19.
+//  Copyright © 2019 Shreya Zala. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
